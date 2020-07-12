d164713360f3896b603da2c9879414818eb1747a
Aaron Mcdonald <aaronmcdonald3d@gmail.com>
2020-07-12 19:05:17 -0400

    added the function to delete a value from the tree

--- Tree.rb
+++ Tree.rb
@@ -1,19 +1,17 @@
 require_relative "Node.rb"
 
 class Tree
-    attr_reader :root, :data
+    attr_reader :root
 
     def initialize(array)
-        @data = build_data(array)
-        @root = build_tree(@data)
+        if(array.length < 1)
+            raise KeyError
+        end
+        @root = build_tree(array)
     end
 
     def build_tree(array)
-        unless(@data == array)
-            arr_data = build_data(array)
-        else
-            arr_data = array
-        end
+        arr_data = build_data(array)
         # base case, array is size 0, return nil
         if(arr_data.length == 0)
             return nil
@@ -40,10 +38,8 @@ class Tree
         # Find the position the node would belong in by starting at the root node and comparing each node as it moves down the tree
         current_node = @root
         node = Node.new(value)
-        repeat_value = false
         while(true)
             if(node == current_node)
-                repeat_value = true
                 break
             elsif(node > current_node)
                 if(current_node.right == nil)
@@ -61,12 +57,56 @@ class Tree
                 end
             end
         end
-        unless(repeat_value)
-            @data.push(value).sort!
+    end
+
+    def delete(value)
+        # Traverse the tree to find a node with a value equal to the one provided, keeping a variable for the previous node traversed
+        prev_node = nil
+        current_node = @root
+        not_found = false
+        side = "none"
+        while(true)
+            if(current_node.value == value)
+                break
+            elsif(current_node.value > value)
+                if(current_node.left == nil)
+                    not_found = true
+                    break
+                else
+                    prev_node = current_node
+                    current_node = current_node.left
+                    side = "left"
+                end
+            else
+                if(current_node.right == nil)
+                    not_found = true
+                    break
+                else
+                    prev_node = current_node
+                    current_node = current_node.right
+                    side = "right"
+                end
+            end
+        end
+        unless(not_found)
+            arr = build_array_from(current_node)
+            arr.delete(value)
+            if(prev_node == nil)
+                @root = build_tree(arr)
+            elsif(side == "right")
+                prev_node.append_right_child(build_tree(arr))
+            else
+                prev_node.append_left_child(build_tree(arr))
+            end
         end
     end
 
+    private
+
     def build_data(array)
+        if(array == nil)
+            return []
+        end
         sorted_array = array.sort
         final = []
         sorted_array.each do |char|
@@ -77,5 +117,22 @@ class Tree
             end
         end
         return final
+    end
+
+    def build_array_from(node)
+        # base case: node == nil, return empty array
+        if(node == nil)
+            return []
+        end
+        arr = Array.new(1, node.value)
+        left_arr = build_array_from(node.left)
+        right_arr = build_array_from(node.right)
+        left_arr.each do |val|
+            arr.push(val)
+        end
+        right_arr.each do |val|
+            arr.push(val)
+        end
+        return arr
     end
 end
--- main.rb
+++ main.rb
@@ -1,10 +1,10 @@
 require_relative "Tree.rb"
 
-tree = Tree.new([1,7,4,23,8,9,4,3,5,7,9,67,6345,324])
+# tree = Tree.new([1,7,4,23,8,9,4,3,5,7,9,67,6345,324])
+tree = Tree.new([23,24,25,26,27])
 
-tree.insert(1)
 p tree.root
-p tree.data
-tree.insert(20)
+
+tree.delete(25)
+
 p tree.root
-p tree.data
